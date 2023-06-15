719ea3f5b2e1c65e0891870b492548f30f854cfb
Anjana <anjana.j28@wipro.com>
2023-06-15 13:31:04 +0530

    Swagger configuration changes

--- src/main/java/com/iemr/tm/config/SwaggerConfig.java
+++ src/main/java/com/iemr/tm/config/SwaggerConfig.java
@@ -26,6 +26,7 @@ import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -37,12 +38,13 @@ public class SwaggerConfig {
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("com.iemr.tm.controller"))
-				/* .paths(regex("/*.*")) */
 				.build().apiInfo(metaData());
 	}
 
 	private ApiInfo metaData() {
-		ApiInfo apiInfo = new ApiInfo("", "", "", "", "", "", "");
+		ApiInfo apiInfo = new ApiInfo("Spring Boot REST API", "Spring Boot REST API for Piramal MMUs application",
+				"1.0", "Terms of service", new Contact("AMRIT", "https://psmri.github.io/PSMRI/", "amrit@piramalswasthya.org"), "",
+				"");
 		return apiInfo;
 	}
 }
